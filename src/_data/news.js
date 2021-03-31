a6abef5690d257c4b402ae97d359fb7168a9aab4
Alexandre Vanini <vanini.alexandre@gmail.com>
2021-03-31 16:00:08 +0200

    Change newsapi api_key name

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines\?country\=fr\&category\=science\&pageSize\=3\&apiKey\=${process.env.newsapi_api_key}`);
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines\?country\=fr\&category\=science\&pageSize\=3\&apiKey\=${process.env.NEWSAPI_API_KEY}`);
       return response.data;
     } catch (error) {
       console.error(error);
